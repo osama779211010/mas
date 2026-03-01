a5d316830b9b9207edc42d39019a68e6e85f4c82
osama779211010 <www.osama779211010@gmail.com>
2026-02-28 16:03:17 -0800

    Update urls.py

--- medical_ai/urls.py
+++ medical_ai/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    MedicalDiagnosisView, ChatAdviceView, 
+    MedicalDiagnosisView, ChatAdviceView, AdminStatsView,
     UserProfileViewSet, DoctorProfileViewSet, BranchViewSet,
     SecretaryProfileViewSet, AppointmentViewSet, ChatMessageViewSet,
     UserViewSet, DiagnosticResultViewSet, RegisterView, LoginView
@@ -22,5 +22,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('predict/', MedicalDiagnosisView.as_view(), name='medical-predict'),
     path('chat-advice/', ChatAdviceView.as_view(), name='chat-advice'),
+    path('stats/', AdminStatsView.as_view(), name='admin-stats'),
     path('', include(router.urls)),
 ]
